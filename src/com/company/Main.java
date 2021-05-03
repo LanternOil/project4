587cf105c22d463399d8d4b203be171311de4b52
LanternOil <spdiii03@gmail.com>
2021-05-03 12:52:38 -0400

    h

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,18 @@
 package com.company;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import static java.util.Collections.*;
@@ -9,17 +20,30 @@ import static java.util.Collections.*;
 public class Main {
 
     public static void main(String[] args) {
+        //The program needs very few global variables, most are for utility.
         Scanner input = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<>();
 
+        //These are responsible for the write to file running on shutdown
+        ShutDownTask shutDownTask = new ShutDownTask();
+        Runtime.getRuntime().addShutdownHook(shutDownTask);
 
-        while (true){
+        //updates the task list on startup.
+        taskList = readFromFile(taskList);
 
+
+        while (true) {
             menu();
-            int userChoice = input.nextInt();
+            int userChoice = 0;
+
+
+            userChoice = input.nextInt();
             input.nextLine();
 
-            switch (userChoice){
+
+            //this large switch case statement calls the different functions.
+
+            switch (userChoice) {
                 //Task Creation
                 case 1:
                     System.out.println("Please give the task a name.");
@@ -29,14 +53,14 @@ public class Main {
                     input.nextLine();
                     System.out.println("Please describe the task.");
                     String desc = input.nextLine();
-                    addTask(taskList,name,prio,desc);
+                    addTask(taskList, name, prio, desc);
                     break;
                 //Task Deletion
                 case 2:
                     System.out.println("Enter the number of the task you would like to delete");
                     listTask(taskList);
                     int i = input.nextInt();
-                    deleteTask(taskList,i);
+                    deleteTask(taskList, i);
                     break;
                 //Update task
                 case 3:
@@ -46,7 +70,7 @@ public class Main {
                     input.nextLine();
                     System.out.println("Please enter the new task description");
                     desc = input.nextLine();
-                    updateTask(taskList,i,desc);
+                    updateTask(taskList, i, desc);
                     break;
                 //List Tasks
                 case 4:
@@ -59,16 +83,52 @@ public class Main {
                     input.nextLine();
                     listPriority(taskList, i);
                     break;
+                //Sorts all the tasks
                 case 6:
                     sortTask(taskList);
                     break;
-                case 0: System.exit(0);
+                //Writes the task list to a json file
+                case 7:
+                    writeToFile(taskList);
+                    break;
+                //Reads and UPDATES the task list, using the json file
+                case 8:
+                    taskList = readFromFile(taskList);
+                    break;
+                //Shuts down the program, and writes the list automatically.
+                case 0:
+                    shutDownTask.setTaskList(taskList);
+                    System.exit(0);
+            }
+
+
+        }
+
+    }
+
+    //Ensures that writelist runs on shutdown
+    private static class ShutDownTask extends Thread {
+
+        private ArrayList writeList;
+
+        public void setTaskList(ArrayList writeList) {
+            this.writeList = writeList;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Performing shutdown");
+            try {
+                Thread.sleep(500);
+                writeToFile(writeList);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
 
     }
 
-    public static void menu(){
+    public static void menu() {
         System.out.println("Please choose an option:");
         System.out.println("(1) Add a task.");
         System.out.println("(2) Remove a task.");
@@ -76,26 +136,66 @@ public class Main {
         System.out.println("(4) List all tasks.");
         System.out.println("(5) List all tasks with a priority");
         System.out.println("(6) Sort the tasks");
+        System.out.println("(7) Write the list to file");
+        System.out.println("(8) Read the list from file");
         System.out.println("(0) Exit.");
     }
 
-    static void addTask(ArrayList tasks, String title, int priority, String desc){
-        tasks.add(new Task(title,priority,desc));
-    }
+    //Writes the task list to a json file
+    static void writeToFile(ArrayList tasks) {
+        Gson gson = new Gson();
+        String json = gson.toJson(tasks);
 
-    static void deleteTask(ArrayList tasks, int index){
-        tasks.remove(index-1);
-    }
+        //System.out.println(json);
 
-    static void listTask(ArrayList tasks){
-        //test
-
-        for (int i =0; i < tasks.size(); i++){
-            System.out.println(i+1 + ": " + tasks.get(i));
+        try {
+            FileWriter writer = new FileWriter("data.json");
+            gson.toJson(tasks, writer);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    static void sortTask(ArrayList<Task> tasks){
+    //Converts the json file back to an arraylist of tasks.
+    static ArrayList<Task> readFromFile(ArrayList<Task> tasks) {
+
+        JsonParser parser = new JsonParser();
+        Gson gson = new Gson();
+        JsonElement list;
+
+        try {
+            list = parser.parse(new FileReader("data.json"));
+            Type listType = new TypeToken<ArrayList<Task>>() {
+            }.getType();
+            tasks = gson.fromJson(list, listType);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
+    //Creates a task object and adds it to the array list
+    static void addTask(ArrayList tasks, String title, int priority, String desc) {
+        tasks.add(new Task(title, priority, desc));
+    }
+
+    //Deletes a task, based on its position in the array.
+    static void deleteTask(ArrayList tasks, int index) {
+        //This accounts for the difference between what is shown to the user, and what the actual index is.
+        tasks.remove(index - 1);
+    }
+
+    //Lists all the tasks
+    static void listTask(ArrayList tasks) {
+        //Adds 1 to the index to be more user friendly
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + 1 + ": " + tasks.get(i));
+        }
+    }
+
+    //Sorts all the tasks alphabetically by priority
+    static void sortTask(ArrayList<Task> tasks) {
         /*for (int i =0; i < tasks.size()+1; i++) {
             if(tasks.get(i).compareTo(tasks.get(i + 1)) < 0);
                 swap(tasks, i, i+1);
@@ -108,11 +208,12 @@ public class Main {
         listTask(tasks);
     }
 
-
-    static void updateTask(ArrayList tasks, int index, String desc){
-        tasks.set(index-1, desc);
+    //Changes certain properties of a task
+    static void updateTask(ArrayList tasks, int index, String desc) {
+        tasks.set(index - 1, desc);
     }
 
+    //lists all tasks based on priority
     static void listPriority(ArrayList<Task> tasks, int userPrio) {
         for (Task item : tasks) {
             if (item.getPriority() == userPrio) {
